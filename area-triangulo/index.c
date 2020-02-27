f110e16c2482dc37a31141f9f4a4e819e95cf1d1
Renner Borges <rennerferreira23@gmail.com>
2020-02-27 19:58:21 -0300

    Area triangulo

---
+++ area-triangulo/index.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    float altura,base,area;
+
+    printf("Informe a base: ");
+    scanf("%f",&base);
+
+    printf("Informe a altura: ");
+    scanf("%f",&altura);
+
+    area = (base*altura)/2;
+
+    printf("A area do triangulo é: %.1f",area);
+
+    return 0;
+}
