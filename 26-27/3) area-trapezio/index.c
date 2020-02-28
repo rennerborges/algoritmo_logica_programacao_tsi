6139824dda0e92f16156bccb3fb187d3139032f7
Renner Borges <rennerferreira23@gmail.com>
2020-02-27 22:17:06 -0300

    Area trapezio

--- 26-27/3) area-trapezio/index.c
+++ 26-27/3) area-trapezio/index.c
@@ -3,5 +3,19 @@
 #include <stdio.h>
 
 int main() {
+    float base1,base2, altura, area;
+
+    printf("Informe a base 1: ");
+    scanf("%f",&base1);
+
+    printf("Informe a base 2: ");
+    scanf("%f",&base2);
+
+    printf("Informe a altura: ");
+    scanf("%f",&altura);
+
+    area = ((base1+base2)*altura)/2;
+
+    printf("A area do trapezio é: %.1f",area);
     return 0;
 }
