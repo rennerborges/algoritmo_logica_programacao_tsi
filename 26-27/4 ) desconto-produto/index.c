f19c066d5ac4cbf51b8c8ad592e5ef290243eec0
Renner Borges <rennerferreira23@gmail.com>
2020-02-27 22:48:52 -0300

    Desconto produto

--- 26-27/4 ) desconto-produto/index.c
+++ 26-27/4 ) desconto-produto/index.c
@@ -3,6 +3,27 @@
 #include <stdio.h>
 
 int main() {
-    
+    char produto[30];
+    float valorUnitario, desconto, valorFinal, valorDesconto;
+
+    printf("Informe o nome do produto: ");
+    gets(produto);
+
+    printf("Informe o valor unitario: ");
+    scanf("%f",&valorUnitario);
+
+    printf("Informe a porcentagem de desconto: ");
+    scanf("%f",&desconto);
+
+    valorDesconto = valorUnitario*desconto/100;
+    valorFinal = valorUnitario-valorDesconto;
+
+    printf("Produto: %s \n", produto);
+    printf("Preco: %.2f \n", valorUnitario);
+    printf("Tava de desconto: %.2f \n", desconto);
+    printf("================ \n");
+    printf("Desconto: %.2f \n", valorDesconto);
+    printf("Valor do produto com desconto: %.2f \n", valorFinal);
+
     return 0;
 }
