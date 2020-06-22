204161c7fd056cb2aa209f94ecfb933e197a1f38
Renner Borges <rennerferreira23@gmail.com>
2020-06-22 12:03:12 -0300

    Resolvendo problema no exemplo de vetores

--- explicacao-comandos/vetor/for-valoresInformado/index.c
+++ explicacao-comandos/vetor/for-valoresInformado/index.c
@@ -4,18 +4,22 @@ int main() {
 
     // Usando for para percorrer um valor estatico ou seja pré-definido
 
-    int a[] = {}, tamanho, i = 0;
+    int tamanho, i = 0;
 
     // Aqui será informado o tamanho que o array terá
 
     printf("Informe o tamanho do array:");
     scanf("%d", &tamanho);
 
+    // Declarando o array com o  tamanho informado
+
+    int a[tamanho];
+
     // Aqui estou salvando o valor do array em cada posição levando em conta o tamanho pre-definido
 
     // Lembre-se que deve usar o < (menor) e não o <= (menor ou igual) pois o array começa em 0
 
-    for (i = 0; i < tamanho; i++){
+    for (i = 0; i < 3; i++){
         printf("Informe um valor inteiro:");
         scanf("%d",&a[i]);
     }
@@ -25,7 +29,7 @@ int main() {
     // Lembre-se que deve usar o < (menor) e não o <= (menor ou igual) pois o array começa em 0
 
 
-    for (i = 0; i < tamanho; i++){
+    for (i = 0; i < 3; i++){
         printf("vetor a[%d] = %d \n", i, a[i]);
     }
 
